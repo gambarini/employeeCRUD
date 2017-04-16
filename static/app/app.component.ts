5a644a16b19c3b148cdada4daa837866f8e97014
Gustavo Gambarini <gambarini@gmail.com>
2017-04-16 15:56:19 +1000

    cleaning files

--- static/app/app.component.ts
+++ static/app/app.component.ts
@@ -3,15 +3,9 @@ import { Component }          from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    
+
     <employees-list></employees-list>
 
-    <!--h1>{{title}}</h1>
-    <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet-->
   `,
   styleUrls: ['./app.component.css']
 })
