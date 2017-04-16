9a2b28f6e64236af7015fc5fb2a9012fb64bead4
Gustavo Gambarini <gambarini@gmail.com>
2017-04-16 15:42:28 +1000

    removing file not needed

--- static/app/employee.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Employee = (function () {
-    function Employee(id, name, age, Position) {
-    }
-    return Employee;
-}());
-exports.Employee = Employee;
-//# sourceMappingURL=employee.js.map
